244e930ef042bb663f8f0275faf84241c8b47870
Neutron17 <neutron17771@gmail.com>
2026-01-27 23:18:48 +0100

    Added mő that follows the cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, Routes, ScrollRestoration} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import ErrPage from "./ErrPage";
